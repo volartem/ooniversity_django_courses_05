f21accfab056ad097a725d74abeb49fd2dcea0ae
volartem <volartem7@gmail.com>
2016-11-23 17:35:16 +0200

    comment index variant of main page

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     url(r'^students/', include('students.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^index/$', views.index, name='index'),
+    # url(r'^index/$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^student_list/$', views.student_list, name='student_list'),
     url(r'^student_detail/$', views.student_detail, name='student_detail')

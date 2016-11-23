9df3dd10580a9ef84fe844c2e508353b62811906
volartem <volartem7@gmail.com>
2016-11-23 16:19:24 +0200

    & quadratic with namespaces

--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'quadratic'
 urlpatterns = [
-    url(r'^results/$', views.quadratic_results, name="results"),
+    url(r'^results/$', views.quadratic_results, name="quadratic_results"),
 ]

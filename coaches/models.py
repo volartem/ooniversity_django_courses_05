61e039c92a6c9b3a94f7f75c0b2ca95c98b54761
volartem <volartem7@gmail.com>
2016-11-25 20:21:38 +0200

    Try Fixing __str__ in coaches/models.py

--- coaches/models.py
+++ coaches/models.py
@@ -12,7 +12,7 @@ class Coach(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.user.first_name
+        return self.user.username
 
     @property
     def full_name(self):

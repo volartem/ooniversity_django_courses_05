f3c859bb0c1e3d0a39edb1e177fb9ca9e2e2ebe0
volartem <volartem7@gmail.com>
2016-12-01 18:02:25 +0200

    KR7.3

--- students/urls.py
+++ students/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'students'
 urlpatterns = [
     url(r'^$', views.list_view, name='list_view'),
-    url(r'^(?P<stud_id>[\d]+)/$', views.detail, name='detail')
+    url(r'^(?P<stud_id>[\d]+)/$', views.detail, name='detail'),
+    url(r'^add/$', views.create, name='add'),
+    url(r'^edit/(?P<student_id>\d+)/$', views.edit, name='edit'),
+    url(r'^remove/(?P<student_id>\d+)/$', views.remove, name='remove')
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import StudentModelForm
 from .models import Student
 from courses.models import Course
+from django.contrib import messages
 # Create your views here.
 
 def list_view(request):
@@ -16,3 +17,38 @@ def list_view(request):
 def detail(request, stud_id):
     student = Student.objects.get(id=int(stud_id))
     return render(request, "students/detail.html", {"student": student})
+
+
+def create(request):
+    if request.POST:
+        model_form = StudentModelForm(request.POST)
+        if model_form.is_valid():
+            model_form.save()
+            messages.success(request, 'Student {0} {1} has been successfully added'.format(
+                                                            model_form.cleaned_data['name'],
+                                                            model_form.cleaned_data['surname']))
+            return redirect('/students/')
+    else:
+        model_form = StudentModelForm()
+    return render(request, "students/add.html", {"model": model_form})
+
+def edit(request, student_id):
+    student = Student.objects.get(id=int(student_id))
+    if request.POST:
+        model_form = StudentModelForm(request.POST, instance=student)
+        if model_form.is_valid():
+            model_form.save()
+            messages.success(request, 'Info on the student has been successfully changed.')
+            return redirect('/students/edit/{0}/'.format(student_id))
+    else:
+        model_form = StudentModelForm(instance=student)
+    return render(request, 'students/edit.html', {"model": model_form})
+
+def remove(request, student_id):
+    student = Student.objects.get(id=int(student_id))
+    if request.POST:
+        student.delete()
+        messages.success(request, 'Info on {0} {1} has been successfully deleted.'.format(student.name, student.surname))
+        return redirect('/students/')
+    else:
+        return render(request, 'students/remove.html', {"model": student})

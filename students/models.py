4b47a6578c66bb166a919e496ceb563d59c4b018
volartem <volartem7@gmail.com>
2016-12-11 08:14:19 +0200

    KR10_2 models

--- students/models.py
+++ students/models.py
@@ -8,7 +8,7 @@ class Student(models.Model):
     name = models.CharField(verbose_name='Student name', max_length=60)
     surname = models.CharField(verbose_name='Student surname', max_length=60)
     date_of_birth = models.DateField()
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     phone = models.CharField(max_length=16, null=True, blank=True)
     address = models.CharField(max_length=255)
     skype = models.CharField(max_length=15, null=True, blank=True)

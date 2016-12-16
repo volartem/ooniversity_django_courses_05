3ea70c573bff209f18fdaa90fe4f17d888270315
volartem <volartem7@gmail.com>
2016-12-16 21:01:35 +0200

    KR11_2

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,107 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Course, Lesson
+from coaches.models import Coach
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class CoursesListTest(TestCase):
+    def test_index_view_with_no_questions(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_course_create(self):
+        course = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+        )
+        self.assertEquals(Course.objects.all().count(), 1)
+
+    def test_page(self):
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_valid_link_main(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Главная')
+
+    def test_valid_link_contacts(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Контакты')
+
+    def test_valid_link_students(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Студенты')
+
+
+class CoursesDetailTest(TestCase):
+    def create_lesson(self, course, order):
+        lesson = Lesson.objects.create(
+            subject='Name of Lesson',
+            description='Description of Lesson',
+            course=course,
+            order=order,
+        )
+        return lesson
+
+    def test_course_detail(self):
+        course1 = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+        )
+        response = self.client.get('/courses/1/')
+        self.assertContains(response, course1.name)
+
+    def test_course_detail_response(self):
+        course = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+        )
+        response = self.client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_lessons_course(self):
+        course = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+        )
+        lesson1 = self.create_lesson(course, 1)
+        lesson2 = self.create_lesson(course, 2)
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertContains(response, lesson1.subject and lesson2.subject)
+
+    def test_add_lesson_link(self):
+        course = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+        )
+        response = self.client.get('/courses/{0}/'.format(course.id))
+        self.assertContains(response, '/courses/{0}/add_lesson'.format(course.id))
+
+    def test_coach_course(self):
+        user = User(first_name='Ivan', last_name='Ivanov')
+        user.save()
+        coach = Coach.objects.create(
+            user=user,
+            date_of_birth='2000-01-02',
+            gender='M',
+            phone=12345678,
+            address='Kharkov',
+            skype='ivan',
+            description='Good coach',
+        )
+        course = Course.objects.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',
+            coach=coach,
+        )
+        response = self.client.get('/courses/{}/'.format(course.id))
+        self.assertContains(response, '/coaches/{}/'.format(coach.id))
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,147 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Student
+from courses.models import Course
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class StudentsListTest(TestCase):
+    def test_list_students(self):
+        response = self.client.get(reverse('students:list_view'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_course_create(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='New York',
+            skype='name_surname',
+        )
+        self.assertEquals(Student.objects.all().count(), 1)
+
+    def test_student_add(self):
+        response = self.client.get('/students/')
+        self.assertContains(response, '/students/add/')
+
+    def test_valid_links(self):
+        response = self.client.get('/students/')
+        self.assertContains(response, 'Главная')
+        self.assertContains(response, 'Контакты')
+        self.assertContains(response, 'Студенты')
+
+    def test_student_link(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='San Francisko',
+            skype='name_surname',
+        )
+
+        response = self.client.get('/students/')
+        self.assertContains(response, '/students/{}/'.format(student.id))
+
+    def test_course_link(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='Kharkov',
+            skype='name_surname',
+        )
+        course = student.courses.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default', )
+        response = self.client.get('/students/')
+        self.assertContains(response, '/courses/{}/'.format(course.id))
+
+
+class StudentsDetailTest(TestCase):
+    def test_students_full_name(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='Moscow',
+            skype='name_surname',
+        )
+        response = self.client.get('/students/{0}/'.format(student.id))
+        self.assertContains(response, student.full_name)
+
+    def test_students_valid_info(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='London',
+            skype='name_surname',
+        )
+        response = self.client.get('/students/{0}/'.format(student.id))
+        self.assertContains(response, 'дата рождения')
+        self.assertContains(response, 'адрес')
+        self.assertContains(response, 'почта')
+        self.assertContains(response, 'телефон')
+        self.assertContains(response, 'логин skype')
+
+    def test_students_courses(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='Milan',
+            skype='name_surname',
+        )
+        course1 = student.courses.create(
+            name='Some Course',
+            short_description='Short description',
+            description='Default',)
+        course2 = student.courses.create(
+            name='Some Course2',
+            short_description='Short description2',
+            description='Default2', )
+        response = self.client.get('/students/{0}/'.format(student.id))
+        self.assertContains(response, '/courses/{0}/'.format(course1.id))
+        self.assertContains(response, '/courses/{0}/'.format(course2.id))
+        self.assertEqual(student.courses.all()[0], course1)
+        self.assertEqual(student.courses.all()[1], course2)
+
+    def test_pages_student_detail(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='Moscow',
+            skype='name_surname',
+        )
+        response = self.client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_links(self):
+        student = Student.objects.create(
+            name='Ivan',
+            surname='Ivanov',
+            date_of_birth='2000-01-01',
+            email='test@email.com',
+            phone=123456789,
+            address='Moscow',
+            skype='name_surname',
+        )
+        response = self.client.get('/students/1/')
+        self.assertContains(response, 'Главная')
+        self.assertContains(response, 'Контакты')
+        self.assertContains(response, 'Студенты')

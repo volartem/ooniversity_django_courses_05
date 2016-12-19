a823195973dc9f1f8a85873cd7ce0a760bdc5a03
volartem <volartem7@gmail.com>
2016-12-19 21:37:00 +0200

    closed database access

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^index/$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^student_list/$', views.student_list, name='student_list'),
-    url(r'^student_detail/$', views.student_detail, name='student_detail')
+    # url(r'^login/$',  views.login, name='login'),
+    # url(r'^logout/$', views.logout, name='logout'),
+    # url(r'^student_list/$', views.student_list, name='student_list'),
+    # url(r'^student_detail/$', views.student_detail, name='student_detail')
 ]
 
 if settings.DEBUG:
--- pybursa/views.py
+++ pybursa/views.py
@@ -1,16 +1,40 @@
-from django.http import HttpResponse, response
+from django.http import HttpResponse, response, HttpResponseRedirect
 from django.shortcuts import render
 from courses.models import Course
+from django.contrib import auth
 
 def index(request):
     courses = Course.objects.all()
     return render(request, "index.html", {'courses': courses})
+    # if request.user.is_authenticated():
+    #     return render(request, "index.html", {'courses': courses})
+    # else:
+    #     return render(request, "index.html" )
 
 def contact(request):
     return render(request, "contact.html")
 
-def student_list(request):
-    return render(request, "student_list.html")
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = auth.authenticate(username=username, password=password)
+#         if user is not None and user.is_active:
+#             # Правильный пароль и пользователь "активен"
+#             auth.login(request, user)
+#             # Перенаправление на "правильную" страницу
+#             return HttpResponseRedirect("/account/loggedin/")
+#         else:
+#             # Отображение страницы с ошибкой
+#             return HttpResponseRedirect("/account/invalid/")
+#     else:
+#         return render(request, 'login.html')
+#
+# def logout(request):
+#     pass
 
-def student_detail(request):
-    return render(request, "student_detail.html")
+# def student_list(request):
+#     return render(request, "student_list.html")
+#
+# def student_detail(request):
+#     return render(request, "student_detail.html")
